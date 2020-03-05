825b7ec1f39fecdf56f50ef1dec56c5ff2734b9a
AnaRey <anareyto27@gmail.com>
2020-03-05 11:19:48 -0500

    Adding role for Cluster.

--- terraform/infra/instance.tf
+++ terraform/infra/instance.tf
@@ -44,10 +44,11 @@ module "instance_rampup_kube" {
   source                      = "../modules/instance"
   aws_instance_ami            = "${var.instance_rampup_ami}"
   aws_instance_type           = "${var.instance_rampup_kube_type}"
-  aws_instance_subnet_id      = "${module.subnet_rampup_5.aws_subnet_id}"
+  aws_instance_subnet_id      = "${module.subnet_rampup_4.aws_subnet_id}"
   aws_instance_name           = "${var.instance_rampup_kube_name}"
   aws_instance_key_name       = "${module.key_pair_rampup.aws_key_pair_name}"
   aws_instance_security_group = ["${module.security_group_kubernetes.aws_security_group_id}"]
   aws_instance_kubespray_role = "kube-master"
+  aws_instance_public_ip      = "true"
 }
 
